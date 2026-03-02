fa7ab45ee8a949c8b58824c62e9f6525971e78da
Gabriela Santos <gabi098827.gs@gmail.com>
2026-03-02 17:48:14 -0300

    atividade concluída.

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Estrutura de Dados - Fatec (2025/1)" << endl;
+	/**cout << "Estrutura de Dados - Fatec (2025/1)" << endl;
 
 	
 	cout << "Tipos Numericos\n";
@@ -68,16 +68,98 @@ int main()
 	dias = idade * 365;
 	horas = dias * 24;
 	minutos = horas  * 60;
-	segundos = minutos * 60
+	segundos = minutos * 60;
 
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
 	cout << "Sua idade em segundos e:" << segundos << endl;
 
-	cout << "Qualquer tecla para sair...";
+	cout << "Qualquer tecla para continuar...";
 	system("pause"); // somente no Windows
+	system("cls"); */
 
 
+
+	/*--------EXERCÍCIO 1:----------------------------------- */
+// Este exercício mostra os limites de int e short e demonstra overflow.
+// Quando somamos INT_MAX + 1, o valor "dá a volta" e se torna INT_MIN.
+
+
+		int soma;
+		cout << "int  max = " << INT_MAX << "\n";
+		cout << "int  min = " << INT_MIN << "\n";
+		cout << "short max = " << SHRT_MAX << "\n";
+
+		soma = INT_MAX + 1;
+
+		cout << soma;
+		cout << endl << endl;
+
+		cout << "Qualquer tecla para continuar...";
+		system("pause"); // somente no Windows
+		system("cls");
+
+		/*--------EXERCÍCIO 2- Conversão de Temperatura*---------*/
+
+		double c, f, k;
+
+		cout << "Escreva a temperatura que deseja converter: " << endl;
+		cin >> c;
+
+		f = c * 1.8 + 32;
+		k = c + 273.15;
+
+		cout << " Temperatura em Fahrenheit:   " << f << endl;
+		cout << " Temperatura em Kelvin:   " << k << endl;
+
+
+		cout << "Qualquer tecla para continuar...";
+		system("pause"); // somente no Windows
+		system("cls");
+		/*--------EXERCÍCIO 3: Calculadora de Segundos -----------*/
+
+		long d, h, m, s, sr;
+
+		cout << "Digite a quantidade em segundos:" << endl;
+		cin >> s;
+
+		d = s / 86400;
+		long resto = s % 86400;
+
+		h = resto / 3600;
+		resto = resto % 3600;
+
+		m = resto / 60;
+		sr = resto % 60;
+
+		cout << "Dias: " << d << endl;
+		cout << "Horas: " << h << endl;
+		cout << "Minutos: " << m << endl;
+		cout << "Segundos: " << sr << endl;
+
+		cout << "Qualquer tecla para continuar...";
+		system("pause"); // somente no Windows
+		system("cls");
+
+		/*EXERCÍCIO 4----- Ponteiro na Prática*/
+
+
+		int numero = 10;
+
+		cout << "Valor original da variavel: " << numero << endl;
+
+		cout << "Endereco da variavel: " << &numero << endl;
+
+		int* ponteiro = &numero;
+
+		*ponteiro = *ponteiro * 2;
+
+		cout << "Novo valor da variavel (dobro): " << numero << endl;
+
+
+
+		cout << "Qualquer tecla para sair...";
+		system("pause"); // somente no Windows
 }
 
